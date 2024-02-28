387e992fe12101bfd985d7aff99f03c4ac48349c
Amirtegar <119921222+Amirtegar@users.noreply.github.com>
2024-02-28 19:46:32 +0700

    Update customer_segmentation_streamlit.py

--- customer_segmentation_streamlit.py
+++ customer_segmentation_streamlit.py
@@ -1,10 +1,6 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
 import plotly.express as px
-import seaborn as sns
-import matplotlib.pyplot as plt
-import altair as alt
 
 
 # Membaca file CSV
